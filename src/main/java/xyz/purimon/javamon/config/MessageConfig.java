0d5c84fe7767ded82a9292f9901807bb92d6067a
xena <kakl2612@gmail.com>
2019-07-12 05:54:27 +0900

    initial commit.

--- src/main/java/xyz/purimon/javamon/config/MessageConfig.java
+++ src/main/java/xyz/purimon/javamon/config/MessageConfig.java
@@ -2,16 +2,17 @@ package xyz.purimon.javamon.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class MessageConfig extends WebMvcConfigurerAdapter {
+public class MessageConfig {
 
     @Bean
     ReloadableResourceBundleMessageSource messageSource() {
         ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-        messageSource.setBasenames("classpath:i18n/messages");
+        messageSource.setBasenames("classpath:/i18n/messages");
         messageSource.setCacheSeconds(0);
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
